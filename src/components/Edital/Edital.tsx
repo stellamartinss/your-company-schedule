9b0db9d824999182807f4ce806a4883ed8ec32ce
stellaraintree <stella.ribeiro@raintreeinc.com>
2024-06-24 12:48:15 -0300

    fix input sizes

--- src/components/Edital/Edital.tsx
+++ src/components/Edital/Edital.tsx
@@ -4,6 +4,7 @@ import { VectoredEditalProps } from '../../models/Edital';
 import { Panel } from 'primereact/panel';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import './edital.css';
 
 const Edital = ({ concursoContent }: VectoredEditalProps) => {
   const [value, setValue] = useState<string>();
@@ -30,7 +31,10 @@ const Edital = ({ concursoContent }: VectoredEditalProps) => {
                   header={
                     <>
                       {isEditMode ? (
-                        <InputText value={item.title}></InputText>
+                        <InputText
+                          value={item.title}
+                          className='w-full'
+                        ></InputText>
                       ) : (
                         <span>{item.title}</span>
                       )}
@@ -40,36 +44,41 @@ const Edital = ({ concursoContent }: VectoredEditalProps) => {
                   {item.subtopics.map((subtopic) => (
                     <Panel
                       header={
-                        <>
+                        <div>
                           {isEditMode ? (
                             <InputText
                               value={subtopic.title}
-                              style={{ width: '' }}
+                              className='w-full'
                             ></InputText>
                           ) : (
                             <span>{subtopic.title}</span>
                           )}
-                        </>
+                        </div>
                       }
                       toggleable
                     >
                       {subtopic.subtopics.map((sub) => (
-                        <div className='flex align-items-center gap-3'>
-                          <span className='flex align-items-center gap-2'>
-                            <>
-                              {isEditMode ? (
-                                <InputText value={sub.title}></InputText>
-                              ) : (
-                                <span className=''>- {sub.title}</span>
-                              )}
-                            </>
-                          </span>
-                          <InputText
-                            value={value}
-                            placeholder='Link do caderno de provas aqui'
-                            onChange={(e) => setValue(e.target.value)}
-                          />
-                          <Button size='small'>Add</Button>
+                        <div className='flex flex-nowrap gap-3 mb-3'>
+                          <div className='w-full'>
+                            {isEditMode ? (
+                              <InputText
+                                value={sub.title}
+                                className='w-full'
+                              ></InputText>
+                            ) : (
+                              <span className=''>- {sub.title}</span>
+                            )}
+                          </div>
+                          <div>
+                            <div className='p-inputgroup flex-1'>
+                              <InputText placeholder='Link caderno de provas' />
+                              <Button
+                                icon='pi pi-link'
+                                className='p-button-secondary'
+                                onChange={(e) => setValue(e.target.value)}
+                              />
+                            </div>
+                          </div>
                         </div>
                       ))}
                     </Panel>
